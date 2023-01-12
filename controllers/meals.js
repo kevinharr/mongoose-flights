5a321532fdafb9c4beaa1239be6dffa73ed544fe
kevinharr <kevinharr@verizon.net>
2023-01-11 23:32:17 -0500

    setup view folder

---
+++ controllers/meals.js
@@ -0,0 +1,14 @@
+import { Meal } from "../models/meal.js"
+
+function newMeal(req, res) {
+    Meal.find({})
+    .then(meals => {
+        res.render('meals/new', {
+            title: 'Add Meal',
+            meals: meals,
+        })
+    })
+    .catch(err => {
+        res.redirect("/meals")
+    })
+}
--- routes/flights.js
+++ routes/flights.js
@@ -21,6 +21,4 @@ router.get('/:id/edit', flightsCtrl.edit)
 
 router.put('/:id', flightsCtrl.update)
 
-export {
-  router
-}
+
